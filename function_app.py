2a928cfe7e58ec8cf97688bfd968e879f894b3a7
aprv-tradelyncs <apoorv.shekher@tuningbill.com>
2024-12-11 14:31:44 +0530

    fc

---
+++ function_app.py
@@ -0,0 +1,13 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+    )
