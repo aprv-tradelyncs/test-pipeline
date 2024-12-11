ab28e6c3d85a865138f638151c1f1f034c50cec7
aprv-tradelyncs <apoorv.shekher@tuningbill.com>
2024-12-11 15:12:22 +0530

    fc4

--- function_app.py
+++ function_app.py
@@ -8,6 +8,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully.. Pass a name in the query string or in the request body for a personalized response.",
+            "Yo!",
             status_code=200
     )
